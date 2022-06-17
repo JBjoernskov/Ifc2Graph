6a074ade0f0c47d49617bd9e8397bd3f74ba8064
Jakob Bjørnskov <jakob.bjornskov@me.com>
2022-06-17 22:54:58 +0200

    Update module naming

--- test.py
+++ test.py
@@ -7,7 +7,9 @@ import ifc2graph.graph as graph
 path = str(Path(__file__).parent)
 project_name = "test"
 ifc_file_name = "test.ifc"
-ifc_file_path = os.path.join(path, "test", ifc_file_name)
+ifc_file_path = os.path.join(path, "test_ifc_files", ifc_file_name)
+
+print(ifc_file_path)
 ifc_file = ifcopenshell.open(ifc_file_path)
 ifc_geometry = ifcgeometry.IfcGeometry(ifc_file, project_name, force_init=True)
 adjacent_space_dict = ifc_geometry.get_adjacent_spaces()
