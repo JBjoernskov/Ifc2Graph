85734ce94e17af1f5ca8e2df656045609e24d209
Jakob Bjørnskov <jakob.bjornskov@me.com>
2022-06-17 22:49:31 +0200

    Update module naming

--- test.py
+++ test.py
@@ -1,17 +1,17 @@
 from pathlib import Path
 import ifcopenshell
 import os
-import Ifc2Graph.IfcGeometry as IfcGeometry
-import Ifc2Graph.Graph as Graph
+import ifc2graph.ifcgeometry as ifcgeometry
+import ifc2graph.graph as graph
 
 path = str(Path(__file__).parent)
 project_name = "test"
 ifc_file_name = "test.ifc"
 ifc_file_path = os.path.join(path, "test", ifc_file_name)
 ifc_file = ifcopenshell.open(ifc_file_path)
-ifc_geometry = IfcGeometry.IfcGeometry(ifc_file, project_name, force_init=True)
+ifc_geometry = ifcgeometry.IfcGeometry(ifc_file, project_name, force_init=True)
 adjacent_space_dict = ifc_geometry.get_adjacent_spaces()
-graph = Graph.Graph(adjacent_space_dict=adjacent_space_dict,
+graph = graph.Graph(adjacent_space_dict=adjacent_space_dict,
                     space_type_name_dict=ifc_geometry.space_type_name_dict,
                     name=project_name)
 graph.generate_graph()
