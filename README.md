eed100a8398ecd4b53b55ea9b643bc286071bae6
Jakob Bjørnskov <jakob.bjornskov@me.com>
2022-06-18 11:47:33 +0200

    Add new ifc-file examples

--- README.md
+++ README.md
@@ -18,7 +18,7 @@ Ifc2Graph currently has two functionalities, divided into the classes IfcGeometr
 The IfcGeometry class extracts the geometry for each IfcSpace entity in the provided IFC-file using [IfcOpenShell](http://ifcopenshell.org/python), which is used to define [Trimesh](https://trimsh.org/trimesh.html) objects. These Trimesh objects are then used to determine which spaces that are adjacent. 
 
 #### Input:
-As input a valid IFC-file must be provided. An example file is found in the "test_ifc_files" folder.
+As input a path to a valid IFC-file must be provided. An example file is found in the "test_ifc_files" folder.
 
 #### Ouput:
 The output is a dictionary with keys corresponding to the Name attribute of the IfcSpace entities contained in the IFC input file.
--- test.py
+++ test.py
@@ -1,19 +1,32 @@
 from pathlib import Path
-import ifcopenshell
 import os
 import ifc2graph.ifcgeometry as ifcgeometry
 import ifc2graph.graph as graph
 
 path = str(Path(__file__).parent)
 project_name = "test"
-ifc_file_name = "test.ifc"
+ifc_file_name = "Office Building.ifc"
+# ifc_file_name = "Residential House.ifc"
 ifc_file_path = os.path.join(path, "test_ifc_files", ifc_file_name)
 
-print(ifc_file_path)
-ifc_file = ifcopenshell.open(ifc_file_path)
-ifc_geometry = ifcgeometry.IfcGeometry(ifc_file, project_name, force_init=True)
+# First the IfcGeometry object is instantiated.
+ifc_geometry = ifcgeometry.IfcGeometry(ifc_file_path, project_name, force_init=True)
+
+# Using the IfcGeometry object, a dictionary is produced. 
+# Here, the keys corresponds to the Name attribute of the IfcSpace entities contained in the IFC input file. 
+# The values of this dictionary are lists containing the Name attributes of the adjacent IfcSpace entities.
 adjacent_space_dict = ifc_geometry.get_adjacent_spaces()
+
+
+
+# To produce an image of the generated graph, a Graph object must be instantiated.
 graph = graph.Graph(adjacent_space_dict=adjacent_space_dict,
                     space_type_name_dict=ifc_geometry.space_type_name_dict,
                     name=project_name)
-graph.generate_graph()
+
+
+# This function saves as a default an image of the generated graph in the .../Ifc2Graph directory.
+# Optionally, a path can be given as argument. 
+# E.g.: 
+# graph.generate_graph(save_dir="path/to/rep")
+graph.generate_graph() 
