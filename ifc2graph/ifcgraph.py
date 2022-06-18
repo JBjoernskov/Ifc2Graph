bde80b26b2cbe24e0ab305ee3d58aad4f0e58d13
Jakob Bjørnskov <jakob.bjornskov@me.com>
2022-06-18 17:01:19 +0200

    Rename module and class

--- ifc2graph/ifcgraph.py
+++ ifc2graph/ifcgraph.py
@@ -2,17 +2,18 @@ import networkx as nx
 import os
 
 
-class Graph():
+class IfcGraph(nx.Graph):
 
     def __init__(self,
                 adjacent_space_dict=None,
                 space_type_name_dict=None,
                 name_to_type_dict=None,
-                name=None):
+                graph_name=None):
         self.adjacent_space_dict = adjacent_space_dict
         self.space_type_name_dict = space_type_name_dict
         self.name_to_type_dict = name_to_type_dict
-        self.name = name
+        self.graph_name = graph_name
+        super().__init__()
 
     # Currently there is no general method to find the story level.
     # This is based on the Name property of the IfcSpace entities and is only valid for the example office building.
@@ -23,14 +24,12 @@ class Graph():
 
     def generate_graph(self, save_dir=None):
         adjacent_rooms_graph = nx.Graph() ###
-        adjacent_rooms_graph_node_attribute_dict = {}
+        graph_node_attribute_dict = {}
 
         # This is a color list for the building stories
         color_list = ["#808B96", "#e99d4e", "#a6cee3", "#b2df8a", "#fddbd0", "#91998e"] #Last is default
 
         for space,adjacent_space_list in self.adjacent_space_dict.items():
-            space = space.replace("Ø","OE")
-
             story_space_idx = self.get_story(space)
             for adjacent_space in adjacent_space_list:
                 if adjacent_space is not "Outside":
@@ -40,25 +39,24 @@ class Graph():
                     else:
                         story_idx = story_space_idx
                     
-                    adjacent_space = adjacent_space.replace("Ø","OE")
-                    adjacent_rooms_graph.add_edge(adjacent_space, space, color=color_list[story_idx])
+                    self.add_edge(adjacent_space, space, color=color_list[story_idx])
                     if self.space_type_name_dict is not None and self.name_to_type_dict is not None:
                         if self.space_type_name_dict[adjacent_space] in self.name_to_type_dict:
-                            adjacent_rooms_graph_node_attribute_dict[adjacent_space] = {"label": self.name_to_type_dict[self.space_type_name_dict[adjacent_space]]}
+                            graph_node_attribute_dict[adjacent_space] = {"label": self.name_to_type_dict[self.space_type_name_dict[adjacent_space]]}
                         else:
-                            adjacent_rooms_graph_node_attribute_dict[adjacent_space] = {"label": self.space_type_name_dict[adjacent_space]}
+                            graph_node_attribute_dict[adjacent_space] = {"label": self.space_type_name_dict[adjacent_space]}
 
                         if self.space_type_name_dict[space] in self.name_to_type_dict:
-                            adjacent_rooms_graph_node_attribute_dict[space] = {"label": self.name_to_type_dict[self.space_type_name_dict[space]]}
+                            graph_node_attribute_dict[space] = {"label": self.name_to_type_dict[self.space_type_name_dict[space]]}
                         else:
-                            adjacent_rooms_graph_node_attribute_dict[space] = {"label": self.space_type_name_dict[space]}
+                            graph_node_attribute_dict[space] = {"label": self.space_type_name_dict[space]}
         min_fontsize = 25
         max_fontsize = 25
 
         min_width = 1
         max_width = 4
 
-        degree_list = [adjacent_rooms_graph.degree(node) for node in adjacent_rooms_graph.nodes]
+        degree_list = [self.degree(node) for node in self.nodes]
         min_deg = min(degree_list)
         max_deg = max(degree_list)
 
@@ -67,8 +65,8 @@ class Graph():
 
         a_width = (max_width-min_width)/(max_deg-min_deg)
         b_width = max_width-a_width*max_deg
-        for node in adjacent_rooms_graph.nodes:
-            deg = adjacent_rooms_graph.degree(node)
+        for node in self.nodes:
+            deg = self.degree(node)
             fontsize = a_fontsize*deg + b_fontsize
             width = a_width*deg + b_width        
 
@@ -76,21 +74,18 @@ class Graph():
             story_idx = self.get_story(node)
             
 
-            if node not in adjacent_rooms_graph_node_attribute_dict:
-                adjacent_rooms_graph_node_attribute_dict[node] = {"fontsize": fontsize, "width": width, "color": color_list[story_idx]}
+            if node not in graph_node_attribute_dict:
+                graph_node_attribute_dict[node] = {"fontsize": fontsize, "width": width, "color": color_list[story_idx]}
             else:
-                adjacent_rooms_graph_node_attribute_dict[node]["fontsize"] = fontsize
-                adjacent_rooms_graph_node_attribute_dict[node]["width"] = width
-                adjacent_rooms_graph_node_attribute_dict[node]["color"] = color_list[story_idx]
+                graph_node_attribute_dict[node]["fontsize"] = fontsize
+                graph_node_attribute_dict[node]["width"] = width
+                graph_node_attribute_dict[node]["color"] = color_list[story_idx]
 
 
-        nx.set_node_attributes(adjacent_rooms_graph, values=adjacent_rooms_graph_node_attribute_dict)
-
-        graph = nx.drawing.nx_pydot.to_pydot(adjacent_rooms_graph)
-        graph.set_node_defaults(shape="circle", width=0.8, fixedsize="shape", margin=0, style="filled", fontname="Helvetica", color="#23a6db66", fontsize=10, colorscheme="oranges9")
-        graph.set_edge_defaults(fontname="Helvetica", penwidth=5, color="#999999", fontcolor="#999999", fontsize=10, weight=3)
-
-        adjacent_rooms_graph = nx.drawing.nx_pydot.from_pydot(graph)
+        nx.set_node_attributes(self, values=graph_node_attribute_dict)
+        nx.set_node_attributes(self, values="Helvetica", name="fontname")
+        nx.set_node_attributes(self, values=5, name="penwidth")
+        nx.set_node_attributes(self, values=10, name="fontsize")
 
         print("Drawing graph...")
         if save_dir is None:
--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import os
 import ifc2graph.ifcgeometry as ifcgeometry
-import ifc2graph.graph as graph
+import ifc2graph.ifcgraph as ifcgraph
 
 path = str(Path(__file__).parent)
 project_name = "test"
@@ -15,18 +15,17 @@ ifc_geometry = ifcgeometry.IfcGeometry(ifc_file_path, project_name, force_init=T
 # Using the IfcGeometry object, a dictionary is produced. 
 # Here, the keys corresponds to the Name attribute of the IfcSpace entities contained in the IFC input file. 
 # The values of this dictionary are lists containing the Name attributes of the adjacent IfcSpace entities.
-adjacent_space_dict = ifc_geometry.get_adjacent_spaces()
-
+adjacent_spaces_dict = ifc_geometry.get_adjacent_spaces()
 
 
 # To produce an image of the generated graph, a Graph object must be instantiated.
-graph = graph.Graph(adjacent_space_dict=adjacent_space_dict,
+ifc_graph = ifcgraph.IfcGraph(adjacent_space_dict=adjacent_spaces_dict,
                     space_type_name_dict=ifc_geometry.space_type_name_dict,
-                    name=project_name)
+                    graph_name=project_name)
 
 
 # This function saves as a default an image of the generated graph in the .../Ifc2Graph directory.
 # Optionally, a path can be given as argument. 
 # E.g.: 
-# graph.generate_graph(save_dir="path/to/rep")
-graph.generate_graph() 
+# graph_.generate_graph(save_dir="path/to/rep")
+ifc_graph.generate_graph() 
